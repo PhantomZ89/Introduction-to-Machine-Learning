f8cd4057edd877dc222bde0561c415e922f33da7
PhantomZ89 <cshumtim@gmail.com>
2018-09-23 22:19:08 -0400

    Changed Regression
    Changed regression for outlier module

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,16 +25,12 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
 
-
-
-
-
-
-
-
-
-
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print "slope (w/outliers):", reg.coef_
+print "r^2 (test w/outliers)", reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -52,12 +48,6 @@ try:
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
-
-
-
 
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
